5f64eba5670b593ffe8d87eddf21c593bcbdfe65
Zhang Shuai <520zhangshuai@gmail.com>
2017-09-30 16:30:02 +0800

    Add notification activity

--- app/src/main/java/com/example/shuaiz/androido/MainActivity.kt
+++ app/src/main/java/com/example/shuaiz/androido/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.shuaiz.androido
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import butterknife.ButterKnife
+import butterknife.OnClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+    }
+
+    @OnClick(R.id.btn_notification_channel)
+    internal fun notificationOnClick() {
+        startActivity(Intent(this, NotificationActivity::class.java))
     }
 }

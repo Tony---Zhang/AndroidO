5324cc9f630b1988a08c54853b838b8ee7ed221c
Zhang Shuai <520zhangshuai@gmail.com>
2017-10-23 15:28:22 +0800

    rename json data

--- app/src/main/java/com/example/shuaiz/androido/model/Model.kt
+++ app/src/main/java/com/example/shuaiz/androido/model/Model.kt
@@ -2,4 +2,4 @@ package com.example.shuaiz.androido.model
 
 
 data class UIModel(val name: String, val alias: String, val type: String)
-data class UI(val activity: List<UIModel>)
+data class UI(val activities: List<UIModel>)
